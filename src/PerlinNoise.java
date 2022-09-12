72d0ec3655cc6e8fed23d1d9ff58847d7fab5781
Nathan <Nathan@DESKTOP-GN3TL23>
2022-09-12 16:59:02 -0400

    :(

--- src/PerlinNoise.java
+++ src/PerlinNoise.java
@@ -4,17 +4,17 @@ import java.awt.image.DataBufferInt;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
-import java.util.Stack;
 import java.util.function.Function;
-
 import javax.imageio.ImageIO;
-
 import dimensional.DimensionalArray;
 import dimensional.DimensionalVector;
 
 public class PerlinNoise {
 
+    static double min = Double.MAX_VALUE, max = Double.MIN_VALUE;
 	public static Function<Double, Double> smoothstep = n -> n < 0 ? 0 : n > 1 ? 1 : 3*Math.pow(n, 2) - 2*Math.pow(n, 3),
 										   linear = n -> n;
 	
@@ -54,29 +54,44 @@ public class PerlinNoise {
 	
 	public void defineCell(int ... indexes) {
 		if(cells.get(indexes) == null) {
-			Stack<Double> stack = new Stack<>();
+			Queue<Double> queue = new LinkedList<>();
 			int ind[] = new int[indexes.length];
 			for(int i = 0; i < ind.length; i++)
 				ind[i] = indexes[i]/cellWidth;
+			//System.out.println(Arrays.toString(ind));
 			
 			for(int i = 0; i < Math.pow(2, ind.length); i++) {
-				double arr[] = new double[dimensions];
+				double offset[] = new double[dimensions];
 				int corner[] = new int[dimensions];
-				for(int j = 0; j < arr.length; j++) {
-					corner[j] = ind[j]+i>>j&1;
-					arr[j] = (double)indexes[j]-corner[j]*cellWidth;
+				for(int j = 0; j < offset.length; j++) {
+				    
+					corner[j] = ind[j]+(i>>j&1);
+				    //System.out.println(indexes[j]+" "+((double)indexes[j]/cellWidth)+" "+corner[j]);
+				    offset[j] = (double)indexes[j]/cellWidth-corner[j];
+					
 				}
-				System.out.println(Arrays.toString(corner)+" "+vectors.get(corner)+" "+vectors.get(corner).getMagnitude()+" "+Arrays.toString(arr)+" "+DimensionalVector.dotProduct(vectors.get(corner), new DimensionalVector(arr)));
-				stack.push(DimensionalVector.dotProduct(vectors.get(corner), new DimensionalVector(arr)));
+				
+				if(Math.abs(DimensionalVector.dotProduct(vectors.get(corner), new DimensionalVector(offset))) > 1)
+				    System.out.println(Arrays.toString(indexes)+" "+i+" "+DimensionalVector.dotProduct(vectors.get(corner), new DimensionalVector(offset)));
+				queue.add(DimensionalVector.dotProduct(vectors.get(corner), new DimensionalVector(offset)));
 			}
 			
+			String log = "";
 			int step = (int)Math.pow(2, dimensions);
-			while(stack.size() > 1) {
-				double n0 = stack.pop(), n1 = stack.pop();
-				stack.push(n0+function.apply(dimensions-(Math.log(step--)/Math.log(2)))*(n1-n0));
+			while(queue.size() > 1) {
+			    log += queue+"\n";
+				double n0 = queue.poll(), n1 = queue.poll();
+				queue.add(n0+function.apply(dimensions-(Math.log(step--)/Math.log(2)))*(n1-n0));
 			}
 			
-			cells.set(stack.pop(), indexes);
+			//if(Math.abs(stack.peek()) > 1)
+			    //System.out.println(stack.peek()+" "+Arrays.toString(ind));
+			min = Math.min(min, queue.peek());
+			max = Math.max(max, queue.peek());
+			
+			if(Math.abs(queue.peek()) > 1)
+			    System.out.println(log+Arrays.toString(ind)+" "+queue.peek());
+			cells.set(queue.poll(), indexes);
 		}
 	}
 	
@@ -112,18 +127,26 @@ public class PerlinNoise {
 			throw new IllegalArgumentException("Only 2 dimensions are supported rn");
 			
 		defineAllCells();
+		for(int i = 0; i < cells.size(0); i++) {
+		    for(int j = 0; j < cells.size(1); j++)
+		        System.out.printf("%6.2f", cells.get(i, j));
+		    System.out.println();
+		}
+		System.out.println(min+" "+max);
 		
 		BufferedImage img = new BufferedImage(cells.size(0), cells.size(1), BufferedImage.TYPE_INT_RGB);
 		int arr[] = ((DataBufferInt)img.getRaster().getDataBuffer()).getData();
 		for(int i = 0; i < arr.length; i++) {
 			int n = (int)(255*cells.getData().get(i));
+			if(n < 0 || n > 255)
+			    System.out.println(n);
 			arr[i] = new Color(n, n, n).getRGB();
 		}
 		return img;
 	}
 	
 	public static void main(String[] args) throws IOException {
-		PerlinNoise test = new PerlinNoise(10, 10, 10);
+		PerlinNoise test = new PerlinNoise(3, 3, 3);
 		System.out.println(test.size(0)+" "+test.size(1));
 		ImageIO.write(test.getImage(), "png", new File("noise.png"));
 	}
